d92ed8dbfb6a1945c98f5b2a5e77131aeedbb3fc
HP-Maksim\Maksim <maksimboikodev@gmail.com>
2022-06-27 16:24:20 +0300

    ex5.2

--- cache.go
+++ cache.go
@@ -3,21 +3,43 @@ package cache
 import "time"
 
 type Cache struct {
+	CacheItem map[string]Item
+}
+type Item struct {
+	itemValue  string
+	itemExpire time.Time
 }
 
 func NewCache() Cache {
-	return Cache{}
+	return Cache{
+		CacheItem: make(map[string]Item),
+	}
 }
 
-func (receiver) Get(key string) (string, bool) {
-
+func (cache *Cache) Get(key string) (string, bool) {
+	if item, ok := cache.CacheItem[key]; ok {
+		if item.itemExpire.IsZero() || time.Now().Before(item.itemExpire) {
+			return item.itemValue, true
+		}
+	}
+	return "", false
 }
 
-func (receiver) Put(key, value string) {
+func (cache *Cache) Put(key, value string) {
+	cache.CacheItem[key] = Item{itemValue: value}
 }
 
-func (receiver) Keys() []string {
+func (cache *Cache) Keys() []string {
+	var result []string
+
+	for key := range cache.CacheItem {
+		if _, ok := cache.Get(key); ok {
+			result = append(result, key)
+		}
+	}
+	return result
 }
 
-func (receiver) PutTill(key, value string, deadline time.Time) {
+func (cache *Cache) PutTill(key, value string, deadline time.Time) {
+	cache.CacheItem[key] = Item{itemValue: value, itemExpire: deadline}
 }
---
+++ go.mod
@@ -0,0 +1,3 @@
+module cashe
+
+go 1.18
